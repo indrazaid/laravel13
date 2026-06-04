0e2233fdf44eae826650cdf46ac4dfcd0cbe3073
indra <irawanindra405@gmail.com>
2026-06-04 10:46:32 +0700

    update templating

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,7 +46,7 @@
 
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Total Prodi
+                                                Total ProdiSSSSSSSSS
                                             </div>
 
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
